62f386e0809ea01b2ce9b70398e3e30fc1e2d861
Jeru-Almeda <119394501+Jeru-Almeda@users.noreply.github.com>
2023-09-14 13:44:09 +0530

    Add files via upload

---
+++ Lab1/3.py
@@ -0,0 +1,7 @@
+a1=int(input(" Enter Angle 1 : "))
+a2=int(input("Enter Angle 2 : "))
+a3=int(input("Enter Angle 3 :"))
+if((a1+a2+a3)==180):
+    print("It is a Traiangle")
+else:
+    print("It is not a Traiangle")
---
+++ Lab1/lab1.py
@@ -0,0 +1,3 @@
+name=input("Enter your name : ")
+degree=input("Enter your degree program : ")
+print("Welcome to NSBM",name,"from",degree)
